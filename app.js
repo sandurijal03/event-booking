1ab9fb9adce05d39a0ba8e36714bddc949271c1f
sandip rijal <sandurijal03@gmail.com>
2020-12-18 00:27:22 +0545

    schema and resolvers

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  '/',
+  graphqlHTTP({
+    schema: buildSchema(`
+      type RootQuery {
+        events: [String]!
+      }
+
+      type RootMutation {
+        createEvent(name: String): String!
+      }
+
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
+    `),
+    rootValue: {
+      events: (parent, args, context, info) => {
+        return ['Romantic', 'Cooking', 'Sailing', 'All night coding'];
+      },
+      createEvent: ({ name }) => {
+        const eventName = name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+    pretty: true,
+  }),
+);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
