916b0dec44bdf0a917675ae57acf257c89c30542
sandip rijal <sandurijal03@hotmail.com>
2020-12-18 17:45:03 +0545

    some refactoring

--- app.js
+++ app.js
@@ -11,6 +11,36 @@ const app = express();
 
 app.use(express.json());
 
+const events = (eventIds) => {
+  return Event.find({ _id: { $in: eventIds } })
+    .then((events) => {
+      return events.map((event) => {
+        return {
+          ...event._doc,
+          _id: event.id,
+          creator: user.bind(this, event.creator),
+        };
+      });
+    })
+    .catch((err) => {
+      throw new err();
+    });
+};
+
+const user = (userId) => {
+  return User.findById(userId)
+    .then((user) => {
+      return {
+        ...user._doc,
+        _id: user.id,
+        createdEvents: events.bind(this, user._doc.createdEvents),
+      };
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
 app.use(
   '/',
   graphqlHTTP({
@@ -22,12 +52,14 @@ app.use(
       description: String!
       price: Float!
       date: String!
+      creator: User!
     }
 
     type User {
       _id: ID!
       email: String!
       password:String
+      createdEvents: [Event!]
     }
     
     input EventInput{
@@ -63,9 +95,14 @@ app.use(
     rootValue: {
       events: (parent, args, context, info) => {
         return Event.find()
+          .populate('creator')
           .then((events) => {
             return events.map((event) => {
-              return { ...event._doc, _id: event.id };
+              return {
+                ...event._doc,
+                _id: event.id,
+                creator: user.bind(this, event._doc.creator),
+              };
             });
           })
           .catch((err) => {
@@ -76,7 +113,7 @@ app.use(
         const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
-          price: args.eventInput.price,
+          price: +args.eventInput.price,
           date: new Date(args.eventInput.date),
           creator: '5fdbd6200f5d98a3e0d8cd33',
         });
@@ -84,7 +121,11 @@ app.use(
         return event
           .save()
           .then((result) => {
-            createdEvent = { ...result._doc, _id: event._doc._id.toString() };
+            createdEvent = {
+              ...result._doc,
+              _id: result._doc._id.toString(),
+              creator: user.bind(this, result._doc.creator),
+            };
             return User.findById('5fdbd6200f5d98a3e0d8cd33');
           })
           .then((user) => {
@@ -98,7 +139,6 @@ app.use(
             return createdEvent;
           })
           .catch((err) => {
-            console.log(err);
             throw err;
           });
       },
