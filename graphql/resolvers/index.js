ee852e6619bf0b85ad8e4e53fc3a988bc4b48fb0
sandip rijal <sandurijal03@hotmail.com>
2020-12-18 22:44:43 +0545

    Adding booking whole section

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 
 const Event = require('../../models/event');
 const User = require('../../models/user');
+const Booking = require('../../models/booking');
 
 const events = async (eventIds) => {
   try {
@@ -20,6 +21,19 @@ const events = async (eventIds) => {
   }
 };
 
+const singleEvent = async (eventId) => {
+  try {
+    const event = await Event.findById(eventId);
+    return {
+      ...event._doc,
+      _id: event.id,
+      creator: user.bind(this, event.creator),
+    };
+  } catch (err) {
+    throw err;
+  }
+};
+
 const user = async (userId) => {
   try {
     const user = await User.findById(userId);
@@ -34,7 +48,18 @@ const user = async (userId) => {
 };
 
 module.exports = {
-  events: async (parent, args, context, info) => {
+  users: async (args) => {
+    try {
+      const users = await User.find().populate('createdEvents');
+      return users.map((user) => {
+        return { ...user._doc, password: null };
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+
+  events: async (args) => {
     try {
       const events = await Event.find().populate('creator');
       return events.map((event) => {
@@ -43,6 +68,24 @@ module.exports = {
           _id: event.id,
           date: new Date(event._doc.date).toISOString(),
           creator: user.bind(this, event._doc.creator),
+        };
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+
+  bookings: async (args) => {
+    try {
+      const bookings = await Booking.find();
+      return bookings.map((booking) => {
+        return {
+          ...booking.event._doc,
+          _id: booking.id,
+          user: user.bind(this, booking._doc, user),
+          event: singleEvent.bind(this, booking._doc.event),
+          createdAt: new Date(booking._doc.createdAt).toISOString(),
+          updatedAt: new Date(booking._doc.updatedAt).toISOString(),
         };
       });
     } catch (err) {
@@ -93,6 +136,37 @@ module.exports = {
       });
       const result = await user.save();
       return { ...result._doc, _id: result.id, password: null };
+    } catch (err) {
+      throw err;
+    }
+  },
+
+  bookEvent: async (args) => {
+    const fetchedEvent = await Event.findOne({ _id: args.eventId });
+    const booking = new Booking({
+      user: '5fdbd6200f5d98a3e0d8cd33',
+      event: fetchedEvent,
+    });
+    const result = await booking.save();
+    return {
+      ...result._doc,
+      _id: result.id,
+      user: user.bind(this, booking._doc, user),
+      event: singleEvent.bind(this, booking._doc.event),
+      createdAt: new Date(result._doc.createdAt).toISOString(),
+      updatedAt: new Date(result._doc.updatedAt).toISOString(),
+    };
+  },
+  cancelBooking: async (args) => {
+    try {
+      const booking = await Booking.findById(args.bookingId).populate('event');
+      const event = {
+        ...booking.event._doc,
+        _id: booking.event.id,
+        creator: user.bind(this, booking.event._doc.creator),
+      };
+      await Booking.deleteOne({ _id: args.bookingId });
+      return event;
     } catch (err) {
       throw err;
     }
--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -1,6 +1,14 @@
 const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
+type Booking {
+  _id: ID!
+  event: Event!
+  user: User!
+  createdAt: String!
+  updatedAt: String!
+}
+
 type Event {
   _id: ID!
   title: String!
@@ -32,14 +40,18 @@ input UserInput {
 type RootQuery {
   events: [Event!]!
   users: [User!]!
+  bookings: [Booking!]!
 }
 
 type RootMutation {
   createEvent(eventInput:EventInput!): Event
   createUser(userInput: UserInput): User
+  bookEvent(eventId: ID!): Booking!
+  cancelBooking(bookingId: ID!): Event!
 }
-  schema {
-    query: RootQuery
-    mutation: RootMutation
-  }
+
+schema {
+  query: RootQuery
+  mutation: RootMutation
+}
 `);
